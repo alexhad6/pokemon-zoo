e96ef7fd0d74908886be0d5795288c2d616471e4
Alexander benjamin <bengo1023@gmail.com>
2018-04-26 16:51:21 -0400

    Update

--- src/Squirtle.java
+++ src/Squirtle.java
@@ -1,4 +1,22 @@
 
-public class Squirtle extends Pokemon implements Domesticated {
+public class Squirtle extends Domesticated {
+
+	public Squirtle(String nickname, int level) {
+		super("Squirtle", nickname, 20, 19.8, Const.WATER, level, new int[] {44, 48, 65, 50, 64, 43}, Const.TACKLE, VINE_WHIP);
+	}
+	
+	public void tick() {
+		while(getExperience() >= experienceAtLevel(getLevel())) {
+			levelUp();
+			
+			if (getLevel() >= 16) {
+				evolve("Wartotle", 39, 49.6, Const.WATER, new int[] {59, 63, 80, 65, 80, 58}, VINE_WHIP, RAZOR_LEAF);
+			}
+			
+			if (getLevel() >= 32) {
+				evolve("Blastoise", 63, 188.5, Const.WATER, new int[] {79, 83, 100, 85, 105, 78}, Const.PETAL_BLIZZARD, Const.SOLAR_BEAM);
+			}
+		}
+	}
 
 }
