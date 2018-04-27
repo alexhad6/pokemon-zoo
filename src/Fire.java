1efea1cc1556d043a4eadc8063a0c438626417cf
alexhad6 <Alex@Alexs-Air.home>
2018-04-26 22:09:46 -0400

    Update

--- src/Fire.java
+++ src/Fire.java
@@ -1,7 +1,6 @@
 public interface Fire {
-	
+
 	public Move EMBER = new Move("Ember", 1.00, Const.FIRE, Const.SPECIAL, 40);
-	
 	public Move FIRE_FANG = new Move("Fire Fang", 0.95, Const.FIRE, Const.PHYSICAL, 65);
-	
+
 }
