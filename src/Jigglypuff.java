edf1c3b318dd76e6f26644eb537b699b7e90eba2
Alexander benjamin <bengo1023@gmail.com>
2018-04-27 11:24:11 -0400

    update

---
+++ src/Jigglypuff.java
@@ -0,0 +1,8 @@
+
+public class Jigglypuff extends Wild implements Normal {
+
+		public Jigglypuff(int level) {
+			super("Jigglypuff", 20, 12.1, Const.NORMAL, level, new int[] {115, 45, 20, 45, 25, 20}, Const.TACKLE, Const.BITE, 306, 2, Const.HP);
+		}
+
+	}
